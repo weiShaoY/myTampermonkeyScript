f283127d5f56237321a54cffbec8de49c797795a
weiShao <1604705673@qq.com>
2025-08-14 16:56:04 +0800

    chore: 🔧暂存

--- src/components/EmbyCatalogedList/index.vue
+++ src/components/EmbyCatalogedList/index.vue
@@ -34,10 +34,6 @@ type PropsType = {
    */
   style?: CSSProperties
 
-  /**
-   *   是否包裹
-   */
-  isWrap?: boolean
 }
 
 const props = withDefaults(defineProps<PropsType>(), {
@@ -45,7 +41,6 @@ const props = withDefaults(defineProps<PropsType>(), {
   height: '',
   radius: 0,
   class: '',
-  isWrap: false,
 })
 
 const isShowCopy = ref(false)
@@ -84,9 +79,7 @@ function openFolder(event: any) {
 </script>
 
 <template>
-  <!-- 详情页 -->
   <div
-    v-if="!isWrap"
     class="group relative z-1000 m-x-auto m-t-2 w-[95%]"
     :class="props.class"
     :style="{
@@ -109,57 +102,52 @@ function openFolder(event: any) {
       />
 
       <div
-        class="relative block flex translate-y-[-4px] transform items-center justify-between rounded-[12px] bg-[#219C90] p-[12px_27px] text-[1.1rem] transition-transform duration-[250ms] duration-[34ms] duration-[600ms] will-change-transform active:translate-y-[-2px] hover:translate-y-[-6px] !text-white active:transition-transform hover:transition-transform"
+        class="relative block translate-y-[-4px] transform rounded-[12px] bg-[#219C90] p-[12px_27px] text-[1.1rem] transition-transform duration-[250ms] duration-[34ms] duration-[600ms] will-change-transform active:translate-y-[-2px] hover:translate-y-[-6px] !text-white active:transition-transform hover:transition-transform"
       >
-        <span
-          class="flex-1 truncate text-start"
-        >
-          {{ video.baseName }}
-        </span>
 
+        <!--  上层 -->
         <div
-          class="flex items-center justify-center"
+          class="mb-1 flex items-center justify-between"
         >
           <div
-            class="m-r-2 flex flex-col text-3"
+            class="flex items-center gap-2 text-4"
           >
-            <div
-              class="m-b-1"
-            >
-              {{ video.resolution }}
-            </div>
 
             <div
               class="text-[#e6683c] font-bold"
             >
               {{ video.size }}
             </div>
+
+            <div
+              class=""
+            >
+              {{ video.resolution }}
+            </div>
           </div>
 
-          <SvgIcon
-            v-for="item in video.tagArray"
-            :key="item"
-            :icon="item"
-            class="m-r-1 !h-5 !w-5 !rounded-0"
-          />
-
-          <!-- 复制 已复制 -->
           <div
-            class="m-l-1"
+            class="flex items-center gap-2"
           >
-
             <SvgIcon
-              v-if="!isShowCopy"
-              icon="copy"
-              class="!h-5 !w-5 !rounded-0"
-            />
-
-            <SvgIcon
-              v-if="isShowCopy"
-              icon="copy"
-              class="!h-5 !w-5 !rounded-0"
+              v-for="item in video.tagArray"
+              :key="item"
+              :icon="item"
+              :size="30"
+              class="m-r-1 !h-5 !w-5 !rounded-0"
             />
           </div>
+        </div>
+
+        <!--  下层 -->
+        <div
+          class="flex items-center justify-between"
+        >
+          <span
+            class="flex-1 truncate text-start text-4"
+          >
+            {{ video.baseName }}
+          </span>
         </div>
       </div>
     </button>
@@ -176,33 +164,6 @@ function openFolder(event: any) {
         class="border border-[#52382f] rounded-3 border-solid bg-[#2a2b2f] p-3"
       >
         <div
-          class="m-b-1 flex flex-wrap items-center gap-3"
-        >
-
-          <SvgIcon
-            v-for="item in video.tagArray"
-            :key="item"
-            :icon="item"
-            class="m-r-1 !h-5 !w-5 !rounded-0"
-          />
-
-        </div>
-
-        <div
-          class="flex items-center justify-between text-white"
-        >
-          <div
-            class="text-4 text-[#e6683c] font-bold"
-          >
-            {{ video.size }}
-          </div>
-
-          <div>
-            {{ video.resolution }}
-          </div>
-        </div>
-
-        <div
           class="p-t-1 text-[#ccc]"
         >
           {{ directoryPath }}
@@ -212,134 +173,6 @@ function openFolder(event: any) {
     </div>
   </div>
 
-  <!--  Javbus 列表页 -->
-  <div
-    v-else
-    class="group relative z-1000 m-x-auto m-t-2 w-[95%]"
-    :class="props.class"
-    :style="{
-      borderRadius: `${radius}px`,
-      height: typeof height === 'number' ? `${height}px` : `${height}`,
-      width: typeof width === 'number' ? `${width}px` : `${width}`,
-      ...style,
-    }"
-  >
-    <button
-      class="relative m-t-2 w-full cursor-pointer touch-manipulation border-none bg-transparent p-0 outline-offset-[4px] transition-filter duration-250 ease-in-out focus-visible:outline-none focus:outline-none hover:brightness-[110%]"
-      @click="videoNameCopyToClipboard"
-    >
-      <span
-        class="absolute left-0 top-0 h-full w-full translate-y-[2px] transform rounded-[12px] bg-black/25 shadow transition-transform duration-[250ms] duration-[34ms] duration-[600ms] will-change-transform active:translate-y-[1px] hover:translate-y-[4px] active:transition-transform hover:transition-transform"
-      />
-
-      <span
-        class="from-[hsl(167deg,from-from-[65%],from-from-13%)]"
-      />
-
-      <div
-        class="relative block translate-y-[-4px] transform rounded-[12px] bg-[#219C90] p-x-3 p-y-2 transition-transform duration-[250ms] duration-[34ms] duration-[600ms] will-change-transform active:translate-y-[-2px] hover:translate-y-[-6px] !text-white active:transition-transform hover:transition-transform"
-      >
-
-        <div
-          class="m-b-2 flex items-center"
-        >
-          <SvgIcon
-            v-for="item in video.tagArray"
-            :key="item"
-            :icon="item"
-            class="m-r-1 !h-5 !w-5 !rounded-0"
-          />
-
-        </div>
-
-        <div
-          class="m-b-1 text-start text-[14px]"
-        >
-          {{ video.baseName }}
-
-        </div>
-
-        <div
-          class="flex items-center justify-between text-3"
-        >
-          <div
-            class="text-[#e6683c] font-bold"
-          >
-            {{ video.size }}
-
-          </div>
-
-          <div
-            class=""
-          >
-            {{ video.resolution }}
-
-          </div>
-
-          <div>
-            <SvgIcon
-              v-if="isShowCopy"
-              icon="copy"
-              class="!h-5 !w-5 !rounded-0"
-            />
-          </div>
-        </div>
-
-      </div>
-    </button>
-
-    <!--  Javbus 列表页  悬浮层 -->
-    <div
-      class="absolute bottom-[100%] w-full origin-left scale-0 cursor-pointer select-text rounded-lg bg-[#fff] p-2 transition-all duration-300 ease-in-out group-hover:scale-100"
-      :style="{
-        boxShadow: 'inset 20px 20px 8px #bebebe, inset -20px -20px 8px #ffffff',
-      }"
-      @click="openFolder"
-    >
-      <div
-        class="border border-[#52382f] rounded-3 border-solid bg-[#2a2b2f] p-2"
-      >
-        <div
-          class="flex justify-between"
-        >
-
-          <div
-            class="flex flex-wrap items-center gap-2"
-          >
-            <SvgIcon
-              v-for="item in video.tagArray"
-              :key="item"
-              :icon="item"
-              class="m-r-1 !h-5 !w-5 !rounded-0"
-            />
-          </div>
-
-        </div>
-
-        <div
-          class="flex justify-between text-white"
-        >
-          <div
-            class="text-4 text-[#e6683c] font-bold"
-          >
-            {{ video.size }}
-          </div>
-
-          <div>
-            {{ video.resolution }}
-
-          </div>
-        </div>
-
-        <div
-          class="break-all p-t-1 text-3 text-[#ccc] leading-4"
-        >
-          {{ directoryPath }}
-        </div>
-      </div>
-
-    </div>
-  </div>
 </template>
 
 <style lang="less" scoped></style>
